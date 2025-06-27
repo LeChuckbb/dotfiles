135678207a083d35e9cb22ba3d99bd5f31209001
Park JaeWook <pjw@i-ve.kr>
2025-06-27 19:30:40 +0900

    wip: video, image 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,92 +12,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <video src="https://video.flas1-2.fna.fbcdn.net/v/t42.1790-2/511192340_616544911454892_8915020493252235241_n.?_nc_cat=107&ccb=1-7&_nc_sid=c53f8f&_nc_ohc=7u7qBCvvuIUQ7kNvwGwhuJS&_nc_oc=Admx2qwVQdKaaK2JblA-QNx2HMJB6hx0LaKy4Pxn8ZiqzjC6C_Fb-ZlVPy9d8Ur9f0S2dsMTny1S-7LtI5FCy50D&_nc_zt=28&_nc_ht=video.flas1-2.fna&_nc_gid=iXlCmSnRcepV0IM9lNDUmA&oh=00_AfPQ5ZRHTZ7PiLtUY4oirxaxk5wsRoY7XIFuYyTT1BvJdw&oe=68626A05" controls autoPlay loop muted/>
+        <video src="https://rr3---sn-q4flrnes.googlevideo.com/videoplayback?expire=1750966093&ei=zS5daJxcgIDhwA-j8tPpDA&ip=172.245.58.23&id=1d61aa7b21e5bc65&itag=22&source=youtube&requiressl=yes&xpc=Eghovf3BOnoBAQ==&met=1750937293,&mh=AU&mm=31&mn=sn-q4flrnes&ms=au&mv=u&mvi=3&pl=23&rms=au,au&susc=daps&obr=googlesyndication.com&ctier=L&mime=video/mp4&vprv=1&rqh=1&dur=14.582&lmt=1744145301195574&mt=1750936693&txp=5432534&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,susc,obr,ctier,mime,vprv,rqh,dur,lmt&sig=AJfQdSswRQIgZzGNib2GtIcMKO-0i3NxgMY81K3IZSa92nNBt0oFt9sCIQDUSzPLDjv02tMvFNVn4vK2Uweo9BbM9KULFRwjBMxa4A==&lsparams=met,mh,mm,mn,ms,mv,mvi,pl,rms&lsig=APaTxxMwRgIhAIdh-yqVknLpExtPrrakwIORVTO-XVg2iESyFweeaKBiAiEAr0MvKez_YQBbuMPwEBsKV3RtKxEXO5ODflTvAYLoPa0=" controls autoPlay loop muted/>
+          <iframe src="https://adstransparency.google.com/?authuser=0&region=KR"/>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
